acef361d11d276069feeef629f8626ca63aae12c
Grayson1999 <bbt1250912@gmail.com>
2022-02-03 21:24:46 +0900

    메서드화 완성본

--- method_crawling.py
+++ method_crawling.py
@@ -22,20 +22,13 @@ class Total_crawling():
 
 
     ## 프로그램 사이트 url 찾기
-    def find_data(self,url,elem_func = False,program_page=""):
+    def find_data(self,url,program_page=""):
         self.html = requests.get(url+program_page).text
         self.htmlAll = bs(self.html,'html.parser')
-        if elem_func == False:
-            return 0
-        self.finded_data = elem_func
-        return self.finded_data
 
     
     ## dic, name_dic 변수에 데이터 추가
     def col_append_data(self, df, grogram_name_func, current_col=[], differ_col = []):
-        if len(current_col) == 0:
-            current_col = df.columns.tolist()
-
         for col in current_col:
             for value in df[col]:
                 self.dic[col].append(value)
@@ -52,7 +45,7 @@ class Total_crawling():
         result_df = pd.DataFrame(self.dic)
         result_df = result_df[sort_columns]
         # result_df = result_df.dropna(axis=0)# 결측값 행 제거
-        result_df.to_csv('./{}.csv'.format(self.sitename), sep=',', na_rep='NaN',encoding="CP949", index=False)
+        result_df.to_csv('./{}.csv'.format(self.sitename), sep=',', na_rep='NaN',encoding="utf-8-sig", index=False)
 
     def sbsports(self):##서부재활체육센터
         sitename = "서부재활체육센터"
@@ -60,7 +53,7 @@ class Total_crawling():
         url = "http://www.sbsports.or.kr/sub/wrcAble.do"
         self.initial_setting(sitename,my_columns)
         self.find_data(url)
-        df_list = self.find_data(url,pd.read_html(self.html,header=0))
+        df_list = pd.read_html(self.html,header=0)
         tb_name_list = [x.text.strip() for x in self.htmlAll.find_all("caption")]
 
         for i in range(len(df_list)):#len(df_list)
@@ -69,7 +62,6 @@ class Total_crawling():
             for name in current_col:
                 df_list[i]=df_list[i].rename({name:name.replace(" ","")},axis=1)
             current_col = df_list[i].columns.tolist()
-            print(current_col)
 
             for col in current_col:
                 if col == "비고":
@@ -93,7 +85,58 @@ class Total_crawling():
             self.col_append_data(df_list[i],tb_name_list[i][:-3]+"프로그램" if tb_name_list[i].find("테이블")!=-1 else tb_name_list[i], current_col = inter_col, differ_col = differ_col)
         self.dic.update(self.name_dic)
         self.dic_to_csv()
-
+    
+    
+    def sbsd(self):#서부산권장애인스포츠센터
+        sitename = "서부산권장애인스포츠센터"
+        my_columns = ["프로그램", "시간", "대상", "요일", "사용료", "정원"]
+        url = "https://www.sbsd.kr/Home/"
+        self.initial_setting(sitename,my_columns)
+        self.find_data(url)
+        program_page = self.htmlAll.find("li",{"class":"cd1 cd1c4"}).find("a")["href"].split("/")[-1]
+        self.find_data(url,program_page=program_page)
+        pages = self.htmlAll.find('nav',{"class":"tabmenu"}).find_all("li",{"class":"cd3"})
+        programURLs = []
+        for elem in pages:
+            programURLs.append(elem.find("a")["href"].split("/")[-1])   ## 페이지 넘버
+        for i in range(len(programURLs)):
+            self.find_data(url,program_page = programURLs[i])
+            initial_df = pd.read_html(self.html,header=0)[0]
+            current_col = initial_df.columns.tolist()
+            for col in current_col:
+                if col == "교육일":
+                    initial_df = initial_df.rename({col:"요일"},axis=1)
+                    col = "요일"
+                for value in initial_df[col]:
+                    self.dic[col].append(value)
+            for _ in range(len(initial_df[current_col[0]].values)):
+                self.name_dic["구분"].append(self.htmlAll.find('div',{'class':'s_subject'}).find("p").text)
+        self.dic.update(self.name_dic)
+        self.dic_to_csv()
+    
+    
+    def bisco(self):#부산한마음스포츠센터
+        sitename = "부산한마음스포츠센터"
+        my_columns = ["프로그램","반","대상","교육일","시간","정원","교육비","비고"]
+        url = "http://hmsports.bisco.or.kr"
+        self.initial_setting(sitename,my_columns)
+        self.find_data(url)
+        program_page = self.htmlAll.find_all("li",{"class":"mn_li1"})[1].find("a")["href"]
+        self.find_data(url,program_page=program_page)
+        pages = self.htmlAll.find_all("ul",{"class":"depth2"})[-1].find_all("a")
+        programURLs = [url["href"] for url in pages]
+        del programURLs[-1]## 통합방과후학교 삭제
+        del programURLs[4]## 실내골프연습장 삭제
+        del programURLs[2]## 피트니스실 삭제      
+        for purl in programURLs:
+            self.find_data(url,program_page=purl)
+            initial_df = pd.read_html(self.html,header=0)[0]
+            current_col = initial_df.columns.tolist()
+            self.col_append_data(initial_df,self.htmlAll.find("div",{"id":"print"}).find('h4',{'class':'ctit'}).text,current_col=current_col)
+        self.dic.update(self.name_dic)
+        self.dic_to_csv()
 
 s = Total_crawling()
 s.sbsports()
+s.sbsd()
+s.bisco()
